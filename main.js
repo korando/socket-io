800884a5081fa70defded2a504f5dd3d7434bed2
h2ovn <h2o_vn@hotmail.com>
2015-10-28 18:02:53 +0700

    begin
    Ban đầu. chạy được

---
+++ main.js
@@ -0,0 +1,102 @@
+﻿var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var clients = [];
+var groups = {};
+var groupCounts = 0;
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8004
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+// tạo mới group code name
+function getGroupCode(){
+	groupCounts++;
+	var t = ( "00000" + groupCounts);
+	t = t.substr(t.length - 6);
+	return "g" + t;
+}
+
+// remove một item trong một goup
+function removeGroupItem(groupName, item){
+	var s = groups[groupName];
+	var indexRemoves = [];
+	if (s != null) {
+		// duyệt các item 
+		for(var  i= s.length -1; i >= 0 ; i--) {
+			if(s[i] == item) {
+				indexRemoves.push(i); 		
+			}
+		}
+		// remove
+		for(var i = 0; i < indexRemoves.length; i++){
+			s.splice(indexRemoves[i], 1);
+		}
+		if (s.length == 0) {
+			groups[groupName] = null;
+		}
+	}
+}
+
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
+
+
+function handler (req, res) {
+  res.writeHead(200);
+  res.end("hello");
+}
+
+io.on('connection', function (socket) {
+	//clients.push(socket);
+	console.log("connection " );
+  socket.on('register', function () {
+  	console.log("register");
+  	// kiem tra scoket nay có group chua
+  	if (socket.data_groupName != null) {
+  		// nếu socket này có group ta cần remove nó ra khỏi group cũ
+  		removeGroupItem(socket.data_groupName, socket);
+  	}
+  	var g = getGroupCode();
+  	console.log("getGroupCode", g);
+    groups[g] = [];
+	groups[g].push(socket);
+	socket.data_groupName = g;	
+    socket.emit('onRegister', { data: g, errorCode : 0 });
+  });
+  socket.on('join', function (groupName) {
+    console.log("join", groupName);
+    if(groups[groupName] == null) {
+    	socket.emit('onJoin', { error: "group not found", errorCode : 1 });
+    }
+    else {
+    	var s = groups[groupName];
+    	for(var  i= 0; i < s.length; i++){
+     		s[i].emit('onJoinOther', { errorCode: 0 });   		
+    	}
+    	s.push(socket);
+    	socket.emit('onJoin', { errorCode : 0 });
+    }
+    socket.data_groupName = groupName;	
+  });
+  socket.on('message', function (data) {
+  	var s = groups[socket.data_groupName];
+	if(socket.data_groupName != null && s != null){		
+    	for(var  i= 0; i < s.length; i++){
+    		if(s[i] != socket) {
+     			s[i].emit('onMessage', { errorCode: 0, data: data });   		
+     		}
+    	}
+	}
+	else{
+		socket.emit('onError', { error: "group not found", errorCode : 1 });
+	}
+  });    
+  socket.on('disconnect', function () {
+  	console.log("disconnect ", socket.data_groupName);
+  	// remove nó ra khỏi group
+	if(socket.data_groupName != null){		
+		removeGroupItem(socket.data_groupName, socket);
+	}
+  });
+});
